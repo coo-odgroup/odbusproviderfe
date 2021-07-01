2f582c60964a673a3f93e4fcb48e1e51a58213a3
neotorand@gmail.com <neotorand@gmail.com>
2021-07-01 14:48:12 +0530

    Clean up and Proper Name Conventions Test Case update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-flat-able app is running!');
+    expect(page.getTitleText()).toEqual('od-bus-provider-fe app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-flat-able'),
+      dir: require('path').join(__dirname, './coverage/od-bus-provider-fe'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
