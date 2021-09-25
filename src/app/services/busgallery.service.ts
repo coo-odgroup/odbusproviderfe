b267c1b53d2de12fc878266c69ef5e5db84a669c
chandraodgroup <76548819+chandraodgroup@users.noreply.github.com>
2021-09-25 16:08:39 +0530

    Bus Gallery CORS Fixed

--- src/app/services/busgallery.service.ts
+++ src/app/services/busgallery.service.ts
@@ -21,38 +21,32 @@ export class BusgalleryService {
   constructor(private httpClient: HttpClient) { }
 
   all(): Observable<any> {
-    return this.httpClient.get<any>(this.apiURL + '/Gallery/',  this.httpOptions)
+    return this.httpClient.get<any>(this.apiURL + '/busGallery',  this.httpOptions)
     .pipe(
       catchError(this.errorHandler)
     )
   }
 
   create(post): Observable<any> {
-    return this.httpClient.post<any>(this.apiURL + '/Gallery/', JSON.stringify(post), this.httpOptions)
+    return this.httpClient.post<any>(this.apiURL + '/busGallery', JSON.stringify(post), this.httpOptions)
     .pipe(
       catchError(this.errorHandler)
     )
   }
   update(id, post): Observable<any> {
-    return this.httpClient.put<any>(this.apiURL + '/Gallery/' + id, JSON.stringify(post), this.httpOptions)
+    return this.httpClient.put<any>(this.apiURL + '/busGallery/' + id, JSON.stringify(post), this.httpOptions)
     .pipe(
       catchError(this.errorHandler)
     )
   }
   delete(id){
-    return this.httpClient.delete<any>(this.apiURL + '/Gallery/' + id, this.httpOptions)
+    return this.httpClient.delete<any>(this.apiURL + '/busGallery/' + id, this.httpOptions)
     .pipe(
       catchError(this.errorHandler)
     )
   }
 
-  chngsts(id, post): Observable<any> {
-    // console.log("hello");
-    return this.httpClient.put<any>(this.apiURL + '/changeStatusGallery/' + id,JSON.stringify(post), this.httpOptions)
-    .pipe(
-      catchError(this.errorHandler)
-    )
-  }
+
 
 
   errorHandler(error) {
