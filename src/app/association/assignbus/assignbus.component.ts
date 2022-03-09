7e9da4e668b3349d3ed2b3623a131b127329f1c3
chandraodgroup <chandra@odgroup.in>
2022-03-09 13:37:22 +0530

    bishal-association assign bus section completed

--- src/app/association/assignbus/assignbus.component.ts
+++ src/app/association/assignbus/assignbus.component.ts
@@ -8,6 +8,8 @@ import { Constants } from '../../constant/constant';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 import { AssociationService } from '../../services/association.service';
 import { BusOperatorService } from './../../services/bus-operator.service';
+import { AssocassignbuoperatorService } from './../../services/assocassignbuoperator.service';
+import {AssocassignbusService} from './../../services/assocassignbus.service';
 import { constant } from 'lodash';
 import { NgxSpinnerService } from "ngx-spinner";
 
@@ -38,11 +40,15 @@ export class AssignbusComponent implements OnInit {
   userRecord: User;
   busoperators: any;
   allAssoc: any;
+  assocBus: any;
+  assocBusRecord: any;
+  buses: any;
 
   constructor(
     private spinner: NgxSpinnerService,
     private http: HttpClient,
     private notificationService: NotificationService,
+    private assocBusService: AssocassignbusService,
     private fb: FormBuilder,
     private AssociationService: AssociationService,
     private busOperatorService: BusOperatorService,
@@ -57,67 +63,24 @@ export class AssignbusComponent implements OnInit {
 
 
   ngOnInit(): void {
-
+    this.loadServices();
     this.spinner.show();
     this.form = this.fb.group({
-      id: [null],
-      name: [null, Validators.compose([Validators.required])],
-      email: [null, Validators.compose([Validators.required])],
-      phone: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
-      location:['', Validators.compose([Validators.required])],
-      president_name:[null],
-      president_phone:[null],
-      general_secretary_name:[null],
-      general_secretary_phone:[null],
-      password: [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.required, Validators.maxLength(10)])]
+      id:[null],
+      assocName: [null, Validators.compose([Validators.required])],
+      bus_id: [null, Validators.compose([Validators.required])],
     });
-
-    this.editform = this.fb.group({
-      id: [null],
-      name: [null, Validators.compose([Validators.required])],
-      email: [null, Validators.compose([Validators.required])],
-      phone: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
-      location:['', Validators.compose([Validators.required])],
-      president_name:[null],
-      president_phone:[null],
-      general_secretary_name:[null],
-      general_secretary_phone:[null],
-    });
-
-    this.pwdform = this.fb.group({
-      id: [null],
-      password: [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.required, Validators.maxLength(10)])],
-      conf_password: [null, Validators.compose([Validators.required])]
-    },
-      { validator: this.checkPasswords });
-
 
     this.formConfirm = this.fb.group({
       id: [null]
     });
     this.searchForm = this.fb.group({
       name: [null],
+      assoc_id:[null],
       rows_number: Constants.RecordLimit,
     });
-    this.search();
-
-    // this.getAll();
-
-    this.loadServices();
+    this.search();  
   }
-
-
-
-  checkPasswords(group: FormGroup) { // here we have the 'passwords' group
-    const password = group.get('password').value;
-    const confirmPassword = group.get('conf_password').value;
-
-    return password === confirmPassword ? null : { notSame: true }
-  }
-
-
-
-
 
   page(label: any) {
     return label;
@@ -129,26 +92,27 @@ export class AssignbusComponent implements OnInit {
     this.spinner.show();
     const data = {
       name: this.searchForm.value.name,
+      assoc_id: this.searchForm.value.assoc_id,
       rows_number: this.searchForm.value.rows_number,
     };
 
     // console.log(data);
     if (pageurl != "") {
-      this.AssociationService.getAllaginationData(pageurl, data).subscribe(
+      this.assocBusService.getAllaginationData(pageurl, data).subscribe(
         res => {
-          this.user = res.data.data.data;
-          this.pagination = res.data.data;
+          this.assocBus = res.data.data;
+          this.pagination = res.data;
           // console.log( this.BusOperators);
           this.spinner.hide();
         }
       );
     }
     else {
-      this.AssociationService.getAllData(data).subscribe(
+      this.assocBusService.getAllData(data).subscribe(
         res => {
-          this.user = res.data.data.data;
-          this.pagination = res.data.data;
-          // console.log( res.data);
+          this.assocBus = res.data.data;
+          this.pagination = res.data;
+          // console.log( this.assocBus);
           this.spinner.hide();
         }
       );
@@ -161,6 +125,7 @@ export class AssignbusComponent implements OnInit {
     this.spinner.show();
     this.searchForm = this.fb.group({
       name: [null],
+      assoc_id:[null],
       rows_number: Constants.RecordLimit,
     });
     this.search();
@@ -170,12 +135,45 @@ export class AssignbusComponent implements OnInit {
   loadServices() {
     this.busOperatorService.readassoc().subscribe(
       res => {
-        // this.busoperators = res.data;
         this.allAssoc = res.data;
-        // console.log(res.data);
       }
     );
+
+    // this.busOperatorService.readAll().subscribe(
+    //   res => {
+    //     this.busoperators = res.data;
+    //     this.busoperators.map((i: any) => { i.operatorData = i.organisation_name + '    (  ' + i.operator_name  + '  )'; return i; });
+    //   }
+    // );
   }
+
+  check_bus()
+  {
+    this.buses=[];
+    const data = {
+      assoc_id: this.form.value.assocName
+    };
+    // console.log(data);
+    this.assocBusService.getbuslist(data).subscribe(
+      res => {
+        // console.log(res);
+        this.buses = res;
+        this.buses.map((i:any) => { i.testing = i.name + ' - ' + i.bus_number ; return i; });
+      }
+    );
+
+  }
+
+  
+  onSelectAll() {
+    const selected = this.buses.map(item => item.id);
+    this.form.get('bus_id').patchValue(selected);
+  }
+  onClearAll() {
+    this.form.get('bus_id').patchValue([]);
+  }
+
+
 
   OpenModal(content) {
     this.modalReference = this.modalService.open(content, { scrollable: true, size: 'xl' });
@@ -184,158 +182,56 @@ export class AssignbusComponent implements OnInit {
     return this.pwdform.controls['conf_password'];
   }
   ResetAttributes() {
+    this.buses=[];
     this.userRecord = {} as User;
     this.form = this.fb.group({
-      id: [null],
-      name: [null],
-      email: [null],
-      phone: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
-      location:['', Validators.compose([Validators.required])],
-      president_name:[null],
-      president_phone:[null],
-      general_secretary_name:[null],
-      general_secretary_phone:[null],
-      password: [null]
+      id:[null],
+      assocName: [null, Validators.compose([Validators.required])],
+      bus_id: [null, Validators.compose([Validators.required])],
     });
-
-    this.editform = this.fb.group({
-      id: [null],
-      name: [null],
-      email: [null],
-      phone: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
-      location:['', Validators.compose([Validators.required])],
-      president_name:[''],
-      president_phone:[''],
-      general_secretary_name:[''],
-      general_secretary_phone:[''],
-    });
-
-    this.pwdform = this.fb.group({
-      id: [null],
-      password: [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.required, Validators.maxLength(10)])],
-      conf_password: [null, Validators.compose([Validators.required])]
-    },
-      { validator: this.checkPasswords });
 
     this.form.reset();
-    this.ModalHeading = "Assign Bus For Association";
+    this.ModalHeading = "Add Bus Operator ";
     this.ModalBtn = "Save";
   }
-
-  // getAll()
-  // {
-  //   this.pc.readAll().subscribe(
-  //     res=>{
-  //       this.url = res.data;
-  //       // console.log(res.data);
-  //     }
-  //   );
-  // }
-
 
   addData() {
 
     this.spinner.show();
 
     const data = {
-      name: this.form.value.name,
-      email: this.form.value.email,
-      phone: this.form.value.phone,
-      password: this.form.value.password,
-      location: this.form.value.location,
-      president_name:this.form.value.president_name,
-      president_phone:this.form.value.president_phone,
-      general_secretary_name:this.form.value.general_secretary_name,
-      general_secretary_phone:this.form.value.general_secretary_phone
+      user_id: this.form.value.assocName,
+      bus_id: this.form.value.bus_id,
+      created_by: localStorage.getItem('USERNAME'),
     };
 
-
-    const updateDate = {
-      name: this.editform.value.name,
-      email: this.editform.value.email,
-      phone: this.editform.value.phone,
-      location: this.editform.value.location,
-      president_name:this.editform.value.president_name,
-      president_phone:this.editform.value.president_phone,
-      general_secretary_name:this.editform.value.general_secretary_name,
-      general_secretary_phone:this.editform.value.general_secretary_phone
-    }
-
-
-    // console.log(this.editform);
-    // return false;
-    let id = this.userRecord?.id;
-    if (id != null) {
-      this.AssociationService.update(id, updateDate).subscribe(
-        resp => {
-          if (resp.status == 1) {
-            this.notificationService.addToast({ title: 'Success', msg: resp.message, type: 'success' });
-            this.modalReference.close();
-            this.ResetAttributes();
-            this.refresh();
-
-          }
-          else {
-            this.notificationService.addToast({ title: 'Error', msg: resp.message, type: 'error' });
-            this.spinner.hide();
-          }
-        }
-      );
-    }
-    else {
-      this.AssociationService.create(data).subscribe(
-        resp => {
-
-          if (resp.status == 1) {
-            this.notificationService.addToast({ title: 'Success', msg: resp.message, type: 'success' });
-            this.modalReference.close();
-            this.ResetAttributes();
-            this.refresh();
-
-
-          }
-          else {
-            this.notificationService.addToast({ title: 'Error', msg: resp.message, type: 'error' });
-            this.spinner.hide();
-          }
-        }
-      );
-
-    }
+    this.assocBusService.create(data).subscribe(
+          resp => {
+            if (resp.status == 1) {
+                      this.notificationService.addToast({ title: 'Success', msg: resp.message, type: 'success' });
+                      this.modalReference.close();
+                      this.ResetAttributes();
+                      this.refresh();
+          
+                    }
+          });
 
   }
 
-
-  editData(id) {
-
-    this.userRecord = this.user[id];
-
-    this.editform.controls.id.setValue(this.userRecord.id);
-    this.editform.controls.name.setValue(this.userRecord.name);
-    this.editform.controls.email.setValue(this.userRecord.email);
-    this.editform.controls.phone.setValue(this.userRecord.phone);
-    this.editform.controls.location.setValue(this.userRecord.location);
-    this.editform.controls.president_name.setValue(this.userRecord.president_name);
-    this.editform.controls.president_phone.setValue(this.userRecord.president_phone);
-    this.editform.controls.general_secretary_name.setValue(this.userRecord.general_secretary_name);
-    this.editform.controls.general_secretary_phone.setValue(this.userRecord.general_secretary_phone);
-
-
-
-    this.ModalHeading = "Edit Association";
-    this.ModalBtn = "Update";
-  }
 
   openConfirmDialog(content, id: any) {
     this.confirmDialogReference = this.modalService.open(content, { scrollable: true, size: 'md' });
-    this.userRecord = this.user[id];
+    this.assocBusRecord = this.assocBus[id];
   }
 
   deleteRecord() {
-
-    
-    let delitem = this.userRecord.id;
-    this.AssociationService.delete(delitem).subscribe(
+    let delitem = this.assocBusRecord.id;
+    const data = {
+      id: this.assocBusRecord.id
+    };
+    // console.log(data);
+    // return;
+    this.assocBusService.delete(data).subscribe(
       resp => {
         if (resp.status == 1) {
           this.notificationService.addToast({ title: 'Success', msg: resp.message, type: 'success' });
@@ -348,59 +244,6 @@ export class AssignbusComponent implements OnInit {
           this.spinner.hide();
         }
       });
-  }
-
-
-  changePassword(id) {
-
-
-    this.userRecord = this.user[id];
-
-    this.ModalHeading = "Edit Password";
-    this.ModalBtn = "Update Password";
-  }
-  updatePassword() {
-    this.spinner.show();
-    let id = this.userRecord?.id;
-    const updateDate = {
-      password: this.pwdform.value.password
-    }
-
-    this.AssociationService.changepwd(id, updateDate).subscribe(
-      resp => {
-        if (resp.status == 1) {
-          this.notificationService.addToast({ title: 'Success', msg: resp.message, type: 'success' });
-          this.modalReference.close();
-          this.ResetAttributes();
-          this.refresh();
-        }
-        else {
-          this.notificationService.addToast({ title: 'Error', msg: resp.message, type: 'error' });
-          this.spinner.hide();
-        }
-      }
-    );
-
-
-  }
-
-  changeStatus(event: Event, stsitem: any) {
-
-    this.spinner.show();
-    this.AssociationService.changestatus(stsitem).subscribe(
-      resp => {
-
-        if (resp.status == 1) {
-          this.notificationService.addToast({ title: 'Success', msg: resp.message, type: 'success' });
-          this.ResetAttributes();
-          this.refresh();
-        }
-        else {
-          this.notificationService.addToast({ title: 'Error', msg: resp.message, type: 'error' });
-          this.spinner.hide();
-        }
-      }
-    );
   }
 
 }
