515a89c88c7cd3056af8c66ea7d4d0ba1c127ce2
chandraodgroup <chandra@odgroup.in>
2022-03-26 13:01:47 +0530

    bishal - daterange removed

--- src/app/setting/agentbookingreport/agentbookingreport.component.ts
+++ src/app/setting/agentbookingreport/agentbookingreport.component.ts
@@ -117,7 +117,7 @@ export class AgentbookingreportComponent implements OnInit {
           this.spinner.hide();
         }
       );
-      console.log(this.rs);
+      // console.log(this.rs);
     }
     else
     {
@@ -228,49 +228,7 @@ export class AgentbookingreportComponent implements OnInit {
 }
 
 
-formatDate(date) {
-  var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
 
-  if (month.length < 2) 
-      month = '0' + month;
-  if (day.length < 2) 
-      day = '0' + day;
-
-  return [year, month, day].join('-');
-}
-onDateSelection(date: NgbDate) {
-  if (!this.fromDate && !this.toDate) {
-    this.searchFrom.controls.rangeFromDate.setValue(date);
-    this.fromDate = date;
-  } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
-    this.toDate = date;
-    this.searchFrom.controls.rangeToDate.setValue(date);
-  } else {
-    this.toDate = null;
-    this.fromDate = date;
-    this.searchFrom.controls.rangeFromDate.setValue(date);
-  }
-}
-
-validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
-  const parsed = this.formatter.parse(input);
-  return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
-}
-
-isHovered(date: NgbDate) {
-  return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
-}
-
-isInside(date: NgbDate) {
-  return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-}
-
-isRange(date: NgbDate) {
-  return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
-}
 
 
 }
