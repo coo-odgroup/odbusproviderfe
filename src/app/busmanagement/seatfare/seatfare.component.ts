b464a2d21bab3626a0d08f32518015ea153ab69e
sahilprogrammer <sahil.odbus@gmail.com>
2026-09-16 12:14:03 +0530

    single seat fare module modify

--- src/app/busmanagement/seatfare/seatfare.component.ts
+++ src/app/busmanagement/seatfare/seatfare.component.ts
@@ -42,10 +42,11 @@ export class SeatfareComponent implements OnInit {
   pagination: any;
 
   public fareRecord: any;
+  groupedFare: any[] = [];
   @ViewChild("addnew") addnew;
   all: any;
 
-  constructor( private spinner: NgxSpinnerService,private http: HttpClient, private busService: BusService, private fb: FormBuilder, config: NgbModalConfig, private modalService: NgbModal, dpconfig: NgbDropdownConfig, private busSeatsService: BusSeatsService, private locationService: LocationService, private notificationService: NotificationService) {
+  constructor(private spinner: NgxSpinnerService, private http: HttpClient, private busService: BusService, private fb: FormBuilder, config: NgbModalConfig, private modalService: NgbModal, dpconfig: NgbDropdownConfig, private busSeatsService: BusSeatsService, private locationService: LocationService, private notificationService: NotificationService) {
     config.backdrop = 'static';
     config.keyboard = false;
     this.ModalHeading = "Add New Bus";
@@ -62,7 +63,8 @@ export class SeatfareComponent implements OnInit {
           source_id: [null],
           destination_id: [null],
           // baseFare:[null],
-          new_fare: [null]
+          new_fare: [null],
+          new_fare_status: [0]
         })
       ])
     });
@@ -76,64 +78,225 @@ export class SeatfareComponent implements OnInit {
     this.modalReference = this.modalService.open(content, { scrollable: true, size: 'xl' });
   }
 
-  
 
+  // sortFareBySeatNumber(data: any[]): any[] {
+
+  //   const sortedSeats: any[] = [];
+
+  //   data.forEach(journey => {
+
+  //     journey.get_bus_seats?.forEach((singleSeats: any) => {
+
+  //       // Ignore seats which don't have seat information
+  //       if (!singleSeats.seats) {
+  //         return;
+  //       }
+
+  //       // Calculate base fare
+  //       const seatFare =
+  //         singleSeats.seats.berthType == "1"
+  //           ? journey.base_seat_fare
+  //           : journey.base_sleeper_fare;
+
+  //       sortedSeats.push({
+  //         id: singleSeats.id,
+  //         seat_number: singleSeats.seats.seatText,
+  //         source_id: journey.source_id,
+  //         destination_id: journey.destination_id,
+  //         baseFare: seatFare,
+  //         new_fare: singleSeats.backup_fare,
+  //         new_fare_status: singleSeats.new_fare_status ?? 0
+  //       });
+
+  //     });
+
+  //   });
+
+  //   // Sort by seat number
+  //   sortedSeats.sort((a, b) => {
+  //     return Number(a.seat_number) - Number(b.seat_number);
+  //   });
+
+  //   return sortedSeats;
+  // }
+
+
+
+
+  // getSeats(id: any) {
+  //   this.busRecord = this.buses[id];
+  //   this.locationService.readAll().subscribe(
+  //     records => {
+  //       //CHANDRA TO BE CHANGED
+
+  //       this.locations = records.data;
+  //       let locs = {};
+  //       for (let loc of this.locations) {
+  //         var key = loc.id;
+  //         locs[key] = loc.name;
+  //       }
+  //       this.locationArray = locs;
+  //       // console.log(this.locationArray);
+  //     }
+  //   );
+  //   this.fareRecord = (<FormArray>this.fareGroup.controls['fareArray']) as FormArray;
+  //   this.fareRecord.clear();
+  //   this.busSeatsService.readAll(this.busRecord.id).subscribe(records => {
+
+  //     // console.log(records);
+  //     // console.log(records);
+  //     this.ticketPrice = records.result;
+  //     let baseSeaterFare = "";
+  //     let baseSleeperFare = "";
+  //     let sourceId = "";
+  //     let destinationId = "";
+  //     for (let journey of this.ticketPrice) {
+
+  //       baseSeaterFare = journey.base_seat_fare;
+  //       baseSleeperFare = journey.base_sleeper_fare;
+  //       sourceId = journey.source_id;
+  //       destinationId = journey.destination_id;
+  //       let seatFare = "";
+  //       for (let singleSeats of journey.get_bus_seats) {
+  //         if (singleSeats.seats != null) {
+  //           seatFare = (singleSeats.seats.berthType == "1") ? baseSeaterFare : baseSleeperFare;
+
+  //         }
+  //         let totalLength = this.fareRecord.length;
+  //         let seatRow: FormGroup = this.fb.group({
+  //           id: [singleSeats.id],
+  //           seat_number: [singleSeats.seats.seatText],
+  //           source_id: [sourceId],
+  //           destination_id: [destinationId],
+  //           baseFare: [seatFare],
+  //           new_fare: [singleSeats.backup_fare],
+  //           new_fare_status: [singleSeats.new_fare_status ?? 0]
+  //         })
+  //         this.fareRecord.insert(totalLength, seatRow);
+  //       }
+
+  //     }
+
+  //     //console.log(this.fareRecord);
+  //   });
+  // }
+
+  sortFareBySeatNumber(data: any[]): any[] {
+
+    const sortedSeats: any[] = [];
+
+    data.forEach(journey => {
+
+      journey.get_bus_seats?.forEach((singleSeats: any) => {
+
+        // Ignore seats without seat information
+        if (!singleSeats.seats) {
+          return;
+        }
+
+        // Calculate base fare
+        const seatFare =
+          singleSeats.seats.berthType == "1"
+            ? journey.base_seat_fare
+            : journey.base_sleeper_fare;
+
+        sortedSeats.push({
+          id: singleSeats.id,
+          seat_number: singleSeats.seats.seatText,
+          source_id: journey.source_id,
+          destination_id: journey.destination_id,
+          baseFare: seatFare,
+          new_fare: singleSeats.backup_fare,
+          new_fare_status: singleSeats.new_fare_status ?? 0
+        });
+
+      });
+
+    });
+
+    // Group/sort all same seat numbers together
+    sortedSeats.sort((a, b) => {
+
+      const seatA = Number(a.seat_number);
+      const seatB = Number(b.seat_number);
+
+      // Numeric seat numbers
+      if (!isNaN(seatA) && !isNaN(seatB)) {
+        return seatA - seatB;
+      }
+
+      // If seat numbers contain text like D9, SL6, etc.
+      return String(a.seat_number).localeCompare(
+        String(b.seat_number),
+        undefined,
+        {
+          numeric: true,
+          sensitivity: 'base'
+        }
+      );
+    });
+
+    return sortedSeats;
+  }
 
   getSeats(id: any) {
+
     this.busRecord = this.buses[id];
+
     this.locationService.readAll().subscribe(
       records => {
-        //CHANDRA TO BE CHANGED
-       
+
         this.locations = records.data;
+
         let locs = {};
+
         for (let loc of this.locations) {
           var key = loc.id;
           locs[key] = loc.name;
         }
+
         this.locationArray = locs;
-        // console.log(this.locationArray);
       }
     );
-    this.fareRecord = (<FormArray>this.fareGroup.controls['fareArray']) as FormArray;
+
+    this.fareRecord = this.fareGroup.get('fareArray') as FormArray;
+
     this.fareRecord.clear();
+
     this.busSeatsService.readAll(this.busRecord.id).subscribe(records => {
 
-      // console.log(records);
-      // console.log(records);
       this.ticketPrice = records.result;
-      let baseSeaterFare = "";
-      let baseSleeperFare = "";
-      let sourceId = "";
-      let destinationId = "";
-      for (let journey of this.ticketPrice) {
 
-        baseSeaterFare = journey.base_seat_fare;
-        baseSleeperFare = journey.base_sleeper_fare;
-        sourceId = journey.source_id;
-        destinationId = journey.destination_id;
-        let seatFare = "";
-        for (let singleSeats of journey.get_bus_seats) {
-          if(singleSeats.seats!=null)
-          {
-          seatFare = (singleSeats.seats.berthType == "1") ? baseSeaterFare : baseSleeperFare;
+      // STEP 1:
+      // Convert all journey seats into one array
+      const sortedSeats = this.sortFareBySeatNumber(this.ticketPrice);
 
-          }
-          let totalLength = this.fareRecord.length;
-          let seatRow: FormGroup = this.fb.group({
-            id: [singleSeats.id],
-            seat_number: [singleSeats.seats.seatText],
-            source_id: [sourceId],
-            destination_id: [destinationId],
-            baseFare: [seatFare],
-            new_fare: [singleSeats.new_fare]
-          })
-          this.fareRecord.insert(totalLength, seatRow);
-        }
+      // STEP 2:
+      // Add sorted seats to FormArray
+      sortedSeats.forEach((seat: any) => {
 
-      }
+        const seatRow: FormGroup = this.fb.group({
 
-      //console.log(this.fareRecord);
+          id: [seat.id],
+
+          seat_number: [seat.seat_number],
+
+          source_id: [seat.source_id],
+
+          destination_id: [seat.destination_id],
+
+          baseFare: [seat.baseFare],
+
+          new_fare: [seat.new_fare],
+
+          new_fare_status: [seat.new_fare_status]
+
+        });
+
+        this.fareRecord.push(seatRow);
+
+      });
+
     });
   }
   updatePrice() {
@@ -191,7 +354,7 @@ export class SeatfareComponent implements OnInit {
     const data = {
       name: this.searchForm.value.name,
       rows_number: this.searchForm.value.rows_number,
-      USER_BUS_OPERATOR_ID:sessionStorage.getItem('BUS_OPERATOR_ID')
+      USER_BUS_OPERATOR_ID: sessionStorage.getItem('BUS_OPERATOR_ID')
     };
 
     // console.log(data);
@@ -200,7 +363,7 @@ export class SeatfareComponent implements OnInit {
         res => {
           this.buses = res.data.data.data;
           this.pagination = res.data.data;
-          this.all =res.data;
+          this.all = res.data;
           this.spinner.hide();
           // console.log( this.BusOperators);
         }
@@ -211,9 +374,9 @@ export class SeatfareComponent implements OnInit {
         res => {
           this.buses = res.data.data.data;
           this.pagination = res.data.data;
-          this.all=res.data;
+          this.all = res.data;
           this.spinner.hide();
-          console.log( res.data);
+          console.log(res.data);
         }
       );
     }
@@ -247,6 +410,24 @@ export class SeatfareComponent implements OnInit {
     /* save to file */
     XLSX.writeFile(wb, this.fileName);
 
+  }
+
+
+  toggleAllFareStatus(event: any): void {
+    const checked = event.target.checked;
+
+    this.fareFormGroup.controls.forEach((control: any) => {
+      control.get('new_fare_status').setValue(checked);
+    });
+  }
+
+  isAllChecked(): boolean {
+    const controls = this.fareFormGroup.controls;
+
+    return controls.length > 0 &&
+      controls.every((control: any) =>
+        control.get('new_fare_status').value === true
+      );
   }
 
 
